b41af7275135a4ba681b51af41cf3d635a27502c
njuzj12 <njuzj12@163.com>
2015-06-21 18:49:24 +0800

    1

---
+++ src/com/nju/andball/GainCoinHandler.java
@@ -0,0 +1,18 @@
+package com.nju.andball;
+
+import org.andengine.engine.handler.IUpdateHandler;
+
+public class GainCoinHandler implements IUpdateHandler {
+
+	@Override
+	public void onUpdate(float pSecondsElapsed) {
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
